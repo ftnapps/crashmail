99b01da1196d74fea47007e3fc7dbd43a95cbded
Robert James Clay <jame@rocasa.us>
2009-09-19 14:17:57 -0400

    Add a v0.72 entry to doc/History.txt for Debian package v0.71-3 source changes.
    Peter Krefting made a number of source code changes for v0.71-3 debian package.
    Include the changes in the Crashmail source as v0.72.

--- src/crashmail/crashmail.h
+++ src/crashmail/crashmail.h
@@ -45,11 +45,11 @@
 #include "filter.h"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 71
+#define VERSION_MINOR 72
 
-#define VERSION "0.71"
+#define VERSION "0.72"
 
-#define TID_VERSION "0.71"
+#define TID_VERSION "0.72"
 
 extern struct jbList PktList;
 extern struct jbList DeleteList;
