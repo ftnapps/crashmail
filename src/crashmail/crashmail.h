2c3e0f342ad200bdeb28164f4ebe9e47f5153874
Robert James Clay <jame@rocasa.us>
2013-02-10 12:38:35 -0500

    Set Crashmail version to v0.75.1
    Bump Crashmail version to v0.75.1 and add a VERSION_PATCH definition.

--- src/crashmail/crashmail.h
+++ src/crashmail/crashmail.h
@@ -46,10 +46,11 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 75
+#define VERSION_PATCH 1
 
-#define VERSION "0.75"
+#define VERSION "0.75.1"
 
-#define TID_VERSION "0.75"
+#define TID_VERSION "0.75.1"
 
 extern struct jbList PktList;
 extern struct jbList DeleteList;
